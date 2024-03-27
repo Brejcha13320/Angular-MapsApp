7be894735d949e1f702e359923e9f483c8fbf080
Jesus David Mejia <davidmejia-2000@outlook.com>
2024-03-27 01:46:43 -0500

    implementacion mapbox

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.scss'
+  styleUrl: './zoom-range-page.component.scss',
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('map') divMap?: ElementRef;
+  zoom: number = 10;
+  map?: Map;
+  currentLngLat: LngLat = new LngLat(-74.5, 40);
 
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      accessToken: environment.mapbox_key,
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+
+    this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  mapListeners() {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (event) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (event) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map?.zoomTo(18);
+    });
+
+    this.map.on('move', (event) => {
+      this.currentLngLat = this.map!.getCenter();
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 }
